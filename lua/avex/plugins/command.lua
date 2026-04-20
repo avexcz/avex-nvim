b278d2fbcfec61167bbc220c2cee57f6405e5df1
amvex <134503984+avexcz@users.noreply.github.com>
2026-04-20 13:23:44 +0530

    updated

--- lua/avex/plugins/command.lua
+++ lua/avex/plugins/command.lua
@@ -4,23 +4,22 @@ if not status_ok then
 end
 
 noice.setup({
-    lsp = { -- Added the missing 'lsp =' key here
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    lsp = {
+        
         override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
         },
-    }, -- This brace now correctly closes the 'lsp' table
-    -- you can enable a preset for easier configuration
-    presets = {
-        bottom_search = true,         -- use a classic bottom cmdline for search
-        command_palette = true,      -- CHANGED TO FALSE: This stops the center popup
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
-    -- add this to ensure notifications look clean
+    presets = {
+        bottom_search = true,        
+        command_palette = true,      
+        long_message_to_split = true, 
+        inc_rename = false,           
+        lsp_doc_border = false,       
+    },
+
     messages = {
         enabled = false,
         view = "notify",              -- default view for messages
